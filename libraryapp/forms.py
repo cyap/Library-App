362791844e986785f43380ab08924f1f79965068
Chester Yap <chesterdyap@gmail.com>
2017-05-04 22:01:12 -0500

    Rewrite Django form templates to include ng-model tags
    Also now inherit from Model objects to adhere to DRY principle.

--- libraryapp/forms.py
+++ libraryapp/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-class BookForm(forms.Form):
-	title = forms.CharField(label="Title")
-	author = forms.CharField(label="Author")
-	isbn = forms.IntegerField(label="ISBN Number") 
-	stock = forms.IntegerField(label="Stock")
-	published = forms.IntegerField(label="Publication Year")
-	genre = forms.IntegerField(label="Genre")
+from .models import Book, Transaction
+
+class BookForm(forms.ModelForm):
+
+	def __init__(self, *args, **kwargs):
+		super(BookForm, self).__init__(*args, **kwargs)
+		# Add custom "ng-model" attribute to each field rendered by the form
+		for field_name, field_object in self.fields.items():
+			field_object.widget.attrs.update({"ng-model":field_name})
+
+	class Meta:
+		model = Book
+		fields = ["title", "author", "isbn", "stock", "published", "genre"]
