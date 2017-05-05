2df5690df3b2c49911374903c1d838c4deb81c9f
Chester Yap <chesterdyap@gmail.com>
2017-05-05 00:40:20 -0500

    ISBN Field Changes
    Type change to CharField because ISBN can start with a ‘0’; unique=True
    for use as identifying key in db transactions

--- libraryapp/models.py
+++ libraryapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-	isbn = models.PositiveIntegerField("ISBN Number")
+	isbn = models.CharField("ISBN Number", max_length=255, unique=True)
 	title = models.CharField(max_length=255)
 	author = models.CharField(max_length=255)
-	stock = models.PositiveIntegerField()
 	published = models.IntegerField("Year Published")
 	genre = models.CharField(max_length=255)
+	stock = models.PositiveIntegerField()
 	issued = models.PositiveIntegerField()
 
 class Transaction(models.Model):
