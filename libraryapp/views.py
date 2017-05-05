f02ca01d1a852ec9fac556cc731ff2367cf2e394
Chester Yap <chesterdyap@gmail.com>
2017-05-04 23:53:35 -0500

    Implement functionality for adding book

--- libraryapp/views.py
+++ libraryapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.template import RequestContext
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+
+import json
 
 from .forms import BookForm
 from .models import Book, Transaction
@@ -13,10 +16,11 @@ def index(request):
 
 def books(request):
 	# Query the database for list of books
-	books = list(Book.objects.all())
+	book_list = list(Book.objects.all())
 
 	# Sample book while database is empty
-	books = [{
+	book_list = [{
+	"fields": {
 		"isbn": 100001,
 		"title": "Test2",
 		"author": "Author2",
@@ -24,13 +28,21 @@ def books(request):
 		"genre": "Western",
 		"stock": 3,
 		"issued": 0
+		}
 	}]
 
 	# Return as JSON objects
-	return JsonResponse(data={"books":books})
+	return JsonResponse(data={"books":book_list})
 
 def add(request):
-	return
+	book = Book(**json.loads(request.body))
+	# TODO: Save book to database
+	book_list = serializers.serialize("json", [book])
+
+	# TODO: Load books from database, in order from most recent to least
+	# book_list = list(Book.objects.all())
+
+	return JsonResponse(data={"books":book_list})
 
 def delete(request):
 	return
--- static/libraryapp.js
+++ static/libraryapp.js
@@ -1,21 +1,24 @@
 var app = angular.module("app", []);
 
-/* Generate table */
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRFToken'] = CSRF;
+}]);
+
 app.controller("booksCtrl", function($scope, $http) {
 
 	$scope.update_books = function() {
+		// Populate table from Django database query
 		$http.get("/books").then(function(response) {
 			$scope.books = response.data.books;	
 		});
 	};
 
 	$scope.add_book = function() {
-		/* Add function */
-		$http.post("/add", {new_book: "insert_data_here"}).then(function(response) {
-			$scope.books = response.data.books;
+		$http.post("/add", $scope.book).then(function(response) {
+			$scope.books = angular.fromJson(response.data.books);
 		});
-		$scope.update_books()
 	};
 
+	// Populate table initially
 	$scope.update_books();
 }) 
