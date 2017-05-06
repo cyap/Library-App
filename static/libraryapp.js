a3d3ada0ea0246fa2b5e814bbda31faf765cf831
Chester Yap <chesterdyap@gmail.com>
2017-05-06 02:15:24 -0500

    Refactor button status to depend on selected_row

--- static/libraryapp.js
+++ static/libraryapp.js
@@ -7,19 +7,18 @@ app.config(['$httpProvider', function($httpProvider) {
 app.controller("books_ctrl", function($scope, $http, $timeout) {
 
 	$scope.initialize = function() {
-
 		$scope.editor = {}
 		$http.get("/books").then($scope.update_view);
-		$scope.toggled = false;
+		$scope.add_open = false;
+		$scope.tr_open = false;
 		$scope.mod_disabled = true;
+		
 	};
 
 	$scope.update_view = function(response) {
-		// Update all relevant Angular models
 		$scope.editor = {}
 		// Populate table from Django database query
 		$scope.books = angular.fromJson(response.data.books);
-		console.log($scope.books);
 		// Fill in errors if any 
 		$scope.errors = angular.fromJson(response.data.errors);;
 	}
@@ -31,7 +30,6 @@ app.controller("books_ctrl", function($scope, $http, $timeout) {
 	$scope.delete_book = function() {
 		var delete_id = $scope.selected_row.children[1].children[0].innerHTML
 		$http.post("/delete", {isbn:delete_id}).then($scope.update_view);
-		$scope.selected_row = undefined;
 	};
 	
 	$scope.edit_book = function() {
@@ -41,38 +39,37 @@ app.controller("books_ctrl", function($scope, $http, $timeout) {
 
 	$scope.edit_update = function(new_stock, book) {
 		$http.post("/edit", {stock:new_stock, isbn:book.fields.isbn}).then($scope.update_view);
-
 	}
 
-
 	$scope.select = function(event) {
+		$scope.tr_open = false;
 		$scope.selected_row = event.target;
 		angular.element($scope.selected_row).addClass("active");
-		// Enable edit / delete buttons only when row is selected
-		$scope.mod_disabled = !$scope.mod_disabled;
-		angular.element(document.querySelectorAll(".mod-button")).removeClass("disabled");
 	}
 
 	$scope.blur = function(event) {
-		// Style button disable - reversed if another row is selected
-		angular.element(document.querySelectorAll(".mod-button")).addClass("disabled");
-
 		// Preserve old row such that styling can occur after delay
 		var old_row = $scope.selected_row;
-		//console.log("blur");
+
 		$timeout(function () {
-			//console.log("onblur");
-			// Actually disable button - delayed so onclick can work
-			$scope.mod_disabled = !$scope.mod_disabled;
+			// Disable button - delayed so onclick can work
 			angular.element(old_row).removeClass("active");
-			//$scope.selected_row = undefined;
-		}, 100)
-		
+			if (old_row == $scope.selected_row) {
+				$scope.selected_row = undefined;
+			}
+		}, 150)
 	}
 
 	$scope.toggle_add = function(event) {
 		// Open / close submission form
-		$scope.toggled = !$scope.toggled;
+		$scope.add_open = !$scope.add_open;
+		$scope.tr_open = false;
+	}
+
+	$scope.toggle_tr = function(event) {
+		$scope.tr_open = !$scope.tr_open;
+		$scope.add_open = false;
+		$scope.transaction_title = $scope.selected_row.children[2].children[0].innerHTML
 	}
 
 	$scope.initialize();
